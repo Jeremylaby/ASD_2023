4a2b5d9173c181a9206f8a8eafe16a10a299c564
Jeremylaby <barycki@student.agh.edu.pl>
2024-11-25 21:53:08 +0100

    dodałem quicksorta

--- Python/Quick_select iteracyjny.py
+++ Python/Quick_select iteracyjny.py
@@ -2,13 +2,12 @@ def partition(left,right,A):
     x=A[right]
     i=left-1
     for j in range(left,right):
-        if A[j]>=x: #quick select rosnący  czyli znajduje k-tą największą liczbe
+        if A[j]<=x: #quick select rosnący  czyli znajduje k-tą najmniejszą liczbe
             i+=1
             A[j],A[i]=A[i],A[j]
     A[right],A[i+1]=A[i+1],A[right]
     return i+1
-def Quick_Select(left,right,A,k):
-    
+def Quick_Select(left,right,A,k):  
     while left<=right:
         pivot=partition(left,right,A)
         if pivot==k-1:
@@ -18,5 +17,15 @@ def Quick_Select(left,right,A,k):
         else:
             left=pivot+1
     return A
+
+
+def Quick_Sort(left,right,A):
+    if left < right:
+        pivot = partition(left, right, A)
+        Quick_Sort(left, pivot - 1, A)
+        Quick_Sort(pivot + 1, right, A)
+    return A
 Tab=[1,54,6,12,8,45,12,3,5,18,45,12,0,67,5,4,674,1,2]
 print(Quick_Select(0,len(Tab)-1,Tab,5))
+print(Quick_Sort(0,len(Tab)-1,Tab))
+print(Tab[4])
